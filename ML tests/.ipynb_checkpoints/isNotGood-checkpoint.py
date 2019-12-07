f54afec093bc69e94b5369b2a12acff695d12062
Sergey <Sergeev_sergey99@mail.ru>
2019-12-07 16:24:52 +0300

    update

--- ML tests/.ipynb_checkpoints/isNotGood-checkpoint.py
+++ ML tests/.ipynb_checkpoints/isNotGood-checkpoint.py
@@ -1,28 +1,16 @@
 import os
-
 import numpy as np
-
 # Neural Network
 from keras.models import load_model
 # Text Vectorizing
 from keras.preprocessing.text import Tokenizer
+import pickle
+# loading
 
-
-model = load_model('model.h5')
-
-tokenizer = Tokenizer(num_words=10000, 
-                      filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n', 
-                      lower=True, 
-                      split=' ', 
-                      char_level=False)
+model = load_model('toxic_model75-20.h5')
+with open('tokenizer75-20.pickle', 'rb') as handle:
+    tokenizer = pickle.load(handle)
 
 def isNotGood(str):
     v = model.predict(tokenizer.texts_to_matrix([str]))
-    if v > 0.7:
-        print("Ругательства")
-    if v <= 0.7 and v > 0.3:
-        print("Средне")
-    if v <= 0.3:
-        print("Конструктивная речь")
-        
-isNotGood("JavaScript")
+    return v
